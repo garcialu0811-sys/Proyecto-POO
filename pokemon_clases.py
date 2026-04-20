dff2bd80df221859852a891c207d6c5417a38c6a
garcialu0811-sys <garcialu0811@gmail.com>
2026-04-20 08:40:01 -0600

    Creacion de Setter y Getters de los atributos privados y una parte de los metodos defender y descansar

--- pokemon_clases.py
+++ pokemon_clases.py
@@ -12,21 +12,39 @@ class Pokemon(ABC):
     def hp_actual(self):
         return self.__hp_actual
     
+    # Setter para no permitir valor negativos en hp
     @hp_actual.setter
-    def hp_actual(self):
-        pass
+    def hp_actual(self, valor):
+        if valor < 0:
+            self.__hp_actual = 0
+        else:
+            self.__hp_actual = valor
 
     @property
     def energia_actual(self):
         return self.__energia_actual
-
+    
+    # Setter que no permite energia negativa ni maxima
     @energia_actual.setter
-    def energia_actual(self):
-        pass
+    def energia_actual(self, valor):
+        if valor < 0:
+            self.__energia_actual = 0
+        else:
+            self.__energia_actual = valor
 
     @abstractmethod
     def atacar(self):
         pass
+
+    def defender(self):
+        costo_ep = 5
+
+        if self.energia_actual >= costo_ep:
+            self.energia_actual -= costo_ep
+        
+    def descansar(self):
+        restaurar_ep = 20
+        self.energia_actual += restaurar_ep
 
 class PokemonAgua(Pokemon):
     def atacar(self, oponente):
